8cf7b1a59c971535d8697ce1a59e17003aa22ff5
GhostMobin <s1810677110@ru.ac.bd>
2023-11-17 15:08:53 +0600

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+
+const Hero = () => {
+    return (
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Let&apos;s Make Class Rescheduling Easy </h1>
+                    <p className="mb-5">This website will make department or college class scheduling and rescheduling a breeze, providing an easy way to maintain and update the class schedule.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./Root";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      // {
+      //   path: "/about",
+      //   element: <About></About>,
+      // },
+      // {
+      //   path: "/contact",
+      //   element: <Contact></Contact>,
+      // }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
