9fac23180ab2c1b0a064008eb13b52cd60d2968c
GhostMobin <s1810677110@ru.ac.bd>
2023-11-19 03:07:34 +0600

    dashboard modal

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,18 +8,63 @@ const Dashboard = () => {
             .then(res => res.json())
             .then(data => setRoutine(data))
     }, [])
+
+
+    const handleShowModal = (classString) => {
+        console.log(classString, typeof classString);
+        classString !== "undefined" ?
+            document.getElementById('classModal1').showModal() :
+            document.getElementById('classModal2').showModal();
+
+    }
     return (
         <div>
             <Helmet>
                 <title>Class Routine | Dashboard</title>
             </Helmet>
             <div className="overflow-x-auto">
-                <h2 className="text-center text-3xl font-bold my-12">Class Routine</h2>
-                <div className="flex justify-center gap-8 my-12">
+                {/* <div className="flex justify-center gap-8 my-12">
                     <button className="btn btn-neutral">Only Tomorrow</button>
                     <button className="btn btn-neutral">Only Specific Day</button>
                     <button className="btn btn-neutral">Only Specific Batch</button>
+                </div> */}
+                <div className="flex justify-between items-center my-8 mx-4">
+                    <h2 className="text-center text-3xl font-bold my-8">Class Routine</h2>
+                    {/* <h3 className="font-bold text-2xl">Class Routine</h3> */}
+                    <div className="join">
+                        <button className="btn join-item">Only Tomorrow</button>
+                        <button className="btn join-item">Only My Classes</button>
+                        <select className="select select-bordered join-item">
+                            <option disabled selected>Specific Day</option>
+                            {/* Sunday = associated with the Sun
+                        Monday = associated with the Moon
+                        Tuesday = from the god Tiw, associated with Mars
+                        Wednesday = from Germanic god Odin
+                        Thursday = from Germanic god of thunder Thor
+                        Friday = from Germanic goddess Frigga associated with Venus
+                        Saturday = associated with Saturn */}
+                            <option value="Sunday">Sunday</option>
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                            <option value="Wednesday">Wednesday</option>
+                            <option value="Thursday">Thursday</option>
+                            <option value="">Everyday</option>
+                        </select>
+                        <select className="select select-bordered join-item">
+                            <option disabled selected>Specific Batch</option>
+                            <option value="1st Year Odd (2023)">1st Year Odd (2023)</option>
+                            <option value="1st Year Even (2022)">1st Year Even (2022)</option>
+                            <option value="2nd Year Even (2022)">2nd Year Even (2022)</option>
+                            <option value="3rd Year Odd (2022)">3rd Year Odd (2022)</option>
+                            <option value="4th Year Odd (2022)">4th Year Odd (2022)</option>
+                            <option value="MSc/M 1st Sem (2020)">MSc/M 1st Sem (2020)</option>
+                            <option value="MSc/M 2nd Sem (2020)">MSc/M 2nd Sem (2020)</option>
+                            <option value="MSc/M 3rd Sem (2020)">MSc/M 3rd Sem (2020)</option>
+                        </select>
+                        <button className="btn join-item btn-neutral">View</button>
+                    </div>
                 </div>
+
                 <table className="table table-zebra">
                     {/* head */}
                     <thead className="text-center text-xl">
@@ -54,7 +99,7 @@ const Dashboard = () => {
                             routine.map((day) => (
                                 <>
                                     <tr key={day.id}>
-                                        <th colSpan="9" className="text-center text-3xl py-4 bg-gray-700 text-white">
+                                        <th colSpan="9" className="text-center text-3xl py-4 bg-gray-800 text-white">
                                             {day.name}
                                         </th>
                                     </tr>
@@ -65,37 +110,37 @@ const Dashboard = () => {
                                                     {batch.name}
                                                 </th>
                                                 <td>
-                                                    <button className="btn btn-ghost btn-block">
+                                                    <button className="btn btn-ghost btn-block" onClick={() => handleShowModal(`${batch.schedule["09:05 - 10:00"]}`)}>
                                                         {batch.schedule["09:05 - 10:00"]}
                                                     </button>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn-ghost btn-block">
+                                                    <button className="btn btn-ghost btn-block" onClick={() => handleShowModal(`${batch.schedule["10:05 - 11:00"]}`)}>
                                                         {batch.schedule["10:05 - 11:00"]}
                                                     </button>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn-ghost btn-block">
+                                                    <button className="btn btn-ghost btn-block" onClick={() => handleShowModal(`${batch.schedule["11:05 - 12:00"]}`)}>
                                                         {batch.schedule["11:05 - 12:00"]}
                                                     </button>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn-ghost btn-block">
+                                                    <button className="btn btn-ghost btn-block" onClick={() => handleShowModal(`${batch.schedule["12:05 - 01:00"]}`)}>
                                                         {batch.schedule["12:05 - 01:00"]}
                                                     </button>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn-ghost btn-block">
+                                                    <button className="btn btn-ghost btn-block btn-disabled" onClick={() => handleShowModal(`${batch.schedule["01:05 - 02:00"]}`)}>
                                                         {batch.schedule["01:05 - 02:00"]}
                                                     </button>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn-ghost btn-block">
+                                                    <button className="btn btn-ghost btn-block" onClick={() => handleShowModal(`${batch.schedule["02:05 - 03:00"]}`)}>
                                                         {batch.schedule["02:05 - 03:00"]}
                                                     </button>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn-ghost btn-block">
+                                                    <button className="btn btn-ghost btn-block" onClick={() => handleShowModal(`${batch.schedule["03:05 - 04:00"]}`)}>
                                                         {batch.schedule["03:05 - 04:00"]}
                                                     </button>
                                                 </td>
@@ -115,6 +160,42 @@ const Dashboard = () => {
 
                 </table>
             </div>
+            <dialog id="classModal1" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg mb-2">Change Class State</h3>
+                    <form>
+                        <select className="select select-bordered w-full max-w-xs">
+                            {/* <option disabled selected>Class State</option> */}
+                            <option selected>Will Happen</option>
+                            <option>Pending</option>
+                            <option>Cancelled</option>
+                        </select>
+
+                    </form>
+                </div>
+            </dialog>
+            <dialog id="classModal2" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg mb-2">Assign Class</h3>
+                    <form>
+                        <select className="select select-bordered w-full max-w-xs">
+                            {/* <option disabled selected>Class State</option> */}
+                            <option selected>Will Happen</option>
+                            <option>Pending</option>
+                            <option>Cancelled</option>
+                        </select>
+
+                    </form>
+                </div>
+            </dialog>
         </div>
     );
 };
